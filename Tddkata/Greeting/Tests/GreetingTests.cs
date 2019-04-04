894f8a54b11694370d54e42fbebb99b8a5235ffa
Jeeva <jeeva.ramu@topcashback.ac.uk>
2019-04-04 23:16:54 +0100

    Added Greeting Tests

---
+++ Tddkata/Greeting/Tests/GreetingTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Greetings.Tests
+{
+    public class GreetingTests
+    {
+        [Fact]
+        public void GivenAName_WhenGreetingInterpolated_GreetingShouldBeAdded() 
+        {
+            // Given 
+            string name = "jeeva";
+            var expectedGreeting = $"Hello, {name}";
+            var greeting = new Greeting();
+
+            // When 
+            var result = greeting.Greet(name);
+
+            // Then
+            result.Should().Be(expectedGreeting);
+        }
+
+        [Fact]
+        public void GivenANameAllInUpperCase_WhenGreetingInterpolated_GreetingShouldBeInUpperCase()
+        {
+            // Given 
+            string name = "JEEVA";
+            var expectedGreeting = $"HELLO, {name}";
+            var greeting = new Greeting();
+
+            // When 
+            var result = greeting.Greet(name);
+
+            // Then
+            result.Should().Be(expectedGreeting);
+        }
+    }
+}
