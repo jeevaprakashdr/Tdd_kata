bcde8e0cb71948a47c8608f2f18dbe3db85b13e1
Jeeva Ramu <Jeeva.Ramu@TopCashback.co.uk>
2020-08-22 08:36:20 +0100

    removed property

--- GildedRoseKata/GildedRose/Item.cs
+++ GildedRoseKata/GildedRose/Item.cs
@@ -5,6 +5,5 @@ namespace GildedRose
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
-        public int something {get;}
     }
 }
